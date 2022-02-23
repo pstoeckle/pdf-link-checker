680f567cee23722b14d84fc990b614f8ad8f52b2
Patrick Stöckle <patrick.stoeckle@tum.de>
2022-02-23 23:32:40 +0100

    :white_check_mark: Test ci mode

---
+++ tests/pdf_test.py
@@ -0,0 +1,55 @@
+"""
+TEst.
+"""
+from pathlib import Path
+from unittest import TestCase, main
+
+from pdf_link_checker.main import app
+from typer.testing import CliRunner
+
+
+class PDFTest(TestCase):
+    runner: CliRunner
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        """
+        Class.
+        """
+        cls.runner = CliRunner()
+
+    def test_ci(self) -> None:
+        """
+        Test.
+        """
+        result = self.runner.invoke(
+            app,
+            [
+                "check-links",
+                "--pdf-file",
+                str(Path("tests", "data", "pdflink.pdf")),
+                "--ci",
+            ],
+        )
+        self.assertEqual(result.exit_code, 1)
+
+    def test_ci_ignore(self) -> None:
+        """
+        Test.
+        """
+        result = self.runner.invoke(
+            app,
+            [
+                "check-links",
+                "--pdf-file",
+                str(Path("tests", "data", "pdflink.pdf")),
+                "--ci",
+                "-I",
+                "http://www.adobe.com/suportservice/devrelations/PDFS/TN5150.PDFMARK.PDF",
+            ],
+        )
+        self.assertEqual(0, result.exit_code)
+
+
+if __name__ == "__main__":
+    main()
