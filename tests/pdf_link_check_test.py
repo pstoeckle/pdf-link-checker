49c961c6ec66791f2a311f8d038627dbf6943586
Patrick Stöckle <patrick.stoeckle@tum.de>
2022-02-25 16:26:45 +0100

    :white_check_mark: Adjust expected results

--- tests/pdf_link_check_test.py
+++ tests/pdf_link_check_test.py
@@ -1,7 +1,9 @@
 """Tests for pdf_linker.check.py"""
 from pathlib import Path
+from unittest import main
 
 import pdf_link_checker.pdf_link_check as plc
+from pdf_link_checker.record import Record
 
 
 def test_get_split_large():
@@ -27,19 +29,28 @@ def test_get_split_small():
 def test_check_pdf_links_long():
     """Function to test crawl for a long PDF (under 13 pages)"""
     file_check = plc.check_pdf_links(Path("tests", "data", "testpdf.pdf"))
-    data_check = [3, "https://en.wikipedia.org/wiki/Fish", 200, "NA"]
+    data_check = Record(
+            page_no=3,
+            url="https://en.wikipedia.org/wiki/Fish",
+            code=200,
+            request_error="NA"
+    )
 
-    assert data_check == file_check[1]
+    assert data_check == file_check[0]
 
 
 def test_check_pdf_links_short():
     """Function to test crawl for a short PDF (under 13 pages)"""
     file_check = plc.check_pdf_links(Path("tests", "data", "pdflink.pdf"))
-    data_check = [
-        1,
-        " http://www.adobe.com/suportservice/devrelations/PDFS/TN5150.PDFMARK.PDF",
-        404,
-        "NA",
-    ]
+    data_check = Record(
+            page_no=1,
+            url=" http://www.adobe.com/suportservice/devrelations/PDFS/TN5150.PDFMARK.PDF",
+            code=404,
+            request_error="NA"
+    )
 
-    assert data_check == file_check[1]
+    assert data_check == file_check[0]
+
+
+if __name__ == "__main__":
+    main()
