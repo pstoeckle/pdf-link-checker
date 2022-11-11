d0128d0698adbd70eb12a6da46d205ba5b0a6d12
Patrick Stöckle <patrick.stoeckle@tum.de>
2022-11-11 15:51:43 +0100

    :bug: Package name has changed

--- pdf_link_checker/__init__.py
+++ pdf_link_checker/__init__.py
@@ -6,12 +6,12 @@ from pkg_resources import get_distribution, DistributionNotFound
 
 try:
     # Change here if project is renamed and does not equal the package name
-    dist_name = "pdf-link-checker"
+    dist_name = "python-pdf-link-checker"
     __version__ = get_distribution(dist_name).version
 except DistributionNotFound:
     __version__ = "unknown"
 finally:
     del get_distribution, DistributionNotFound
-__author__ = "Matt Briggs"
-__copyright__ = "Matt Briggs"
+__author__ = "Matt Briggs, Patrick Stöckle"
+__copyright__ = "Matt Briggs, Patrick Stöckle"
 __license__ = "mit"
